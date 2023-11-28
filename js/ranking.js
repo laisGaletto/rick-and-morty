651d07f7852f4c4433b38f968d4278706a8a9770
ramon-miranda <144544269+ramon-miranda@users.noreply.github.com>
2023-11-27 21:40:20 -0300

    Update ranking.js

--- js/ranking.js
+++ js/ranking.js
@@ -1,4 +1,28 @@
-function buscar() {
-    alert("Busca Realizada com sucesso");
-}
+ async function search() {
+    try {
+      let url = await fetch(
+        "https://rickandmortyapi.com/api/character/?name=" + inpSearch.value
+      );
 
+      let dadosSearch = await url.json();
+
+      for (let i of dadosSearch.results) {
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = `
+            <img src="${i.image}" alt="${i.name}"<br> 
+            <h3>${i.name}</h3>
+            <h4 class="status">${i.status}</h4>
+            <p> 👽 ${i.species}  ⚧️${i.gender} </p> 
+                <p> 🌎 ${i.location.name}  </p>
+            `;
+        newDiv.classList.add("card-caracters");
+        resultsContainer.appendChild(newDiv);
+      }
+    } catch (err) {
+      resultsContainer.innerHTML =
+        "<div class='not-found'>Personagem não encontrado</div>";
+    }
+  }
+  //Chama a função Search
+  search();
+});
